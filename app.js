31a18ef3871aefec1ef3dec999556928ef4332b8
ranjith <ranjithvcc@gmail.com>
2024-05-13 08:26:21 +0000

    solution

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const databasePath = path.join(__dirname, "moviesData.db");
+
